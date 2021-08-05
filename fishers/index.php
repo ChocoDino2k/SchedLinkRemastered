500dbfaac35cbadc94ceb22041fa2407982ba992
ChocoDinoMisterSwag <coverlog555@gmail.com>
2021-08-05 10:42:11 -0400

    setup

---
+++ fishers/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="HandheldFriendly" content="true">
+    <title></title>
+    <script src="/global/js/DOM.js" charset="utf-8"></script>
+  </head>
+  <body>
+   <div class="wrapper">
+    <?php
+    $r = $_SERVER['DOCUMENT_ROOT'];
+    include_once($r . "/global/modules/noscript.html");
+    include_once( $r . "/global/modules/header.html");
+   ?>
+   <article class="">
+
+   </article>
+    <?php
+      include_once($r . "/global/modules/navigation.html");
+    ?>
+   </div>
+  </body>
+</html>
---
+++ global/js/DOM.js
@@ -0,0 +1,27 @@
+//returns HTML element specified. Can overload arguments to give the element attributes. Syntax is "attr:value"
+function createElement(type){
+    let elm = document.createElement(type),
+    a;
+    for(let i = 1; i < arguments.length; i++){
+      a = arguments[i].split(":");
+      elm.setAttribute(a[0], a[1]);
+    }
+    return elm;
+}
+//returns all HTML elements within the parent. findAll specifies if all elements of the same name should be found. Overload arguments with element(s) descriptor
+function findElements(parent, findAll, desc){
+  if(parent == undefined || desc == undefined){return;}
+  let elms = [];
+  for(let i = 2; i < arguments.length; i++){
+    if(findAll){
+      let q = parent.querySelectorAll(arguments[i]);
+      for(let thing of q){
+        elms.push(thing);
+      }
+    }else{
+      elms.push(parent.querySelector(arguments[i]));
+    }
+  }
+
+  return (elms.length > 1)? elms : elms[0];
+}
